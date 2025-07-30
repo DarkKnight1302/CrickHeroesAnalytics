8536d26bcb8bc285e4ba252574f22c62ae0d448f
Robin <robin.parashar@yahoo.in>
2025-07-30 21:22:21 +0530

    Modified grounds

--- Constants/GroundList.cs
+++ Constants/GroundList.cs
@@ -7,15 +7,11 @@
             { "srrc1", 6 },
             { "srrc2", 6 },
             { "nithm-cricket-ground", 7 },
-            { "vbcc-1-vattinagulapally", 9 }, // 9km
             { "playsfit-cricket-ground-vattinagulapally", 10 }, // 9.5 km
-            { "swaroop-cricket-ground-kokapet", 11 }, // 11 km
             { "primearena", 12 },
             { "samruddhi-cricket-ground", 12 }, //12 km
             { "the-pavilion-cricket-stadium", 14 }, // 14 km
-            { "vscg-cricket-ground", 14 }, // 14 km
             { "thrill-cricket-ground-1-appa-junction", 15 }, // 15 km
-            { "thrill-cricket-ground-2-appa-junction", 40},
             { "pmr-grounds1", 19 },
             { "pmr-grounds", 19 },
             { "am-cricket-ground", 19 }, // 19 km
@@ -38,23 +34,20 @@
             { "soc-cricket-ground", 24 },
             { "adithya-cricket-grounds(acg)", 25},
             { "olympus-zeus", 25 }, // 25 km
-            { "sr-cricket-ground", 25 }, //25 km
             { "sr2-cricket-ground", 25 }, // 25 km
             { "s.a.r-cricket-arena", 25 }, // 25 km
             { "s.a.r-cricket-arena--ground---1", 25 }, // 25 km
+            { "ballebaaz-saudaan", 26},
             { "olympus-cricket-ground", 25 }, // 25 km
             { "warriors-ground", 27 },
             { "sukruth-cricket-ground", 30 }, // 27 km
             { "hitman-azhit-cricket-ground", 27 }, // 27 km
             { "rao's-cricket-ground", 27 }, // 27km
             { "msk-cricket-ground-vattinagulapally", 40 }, // 14 km
-            { "vvr2-cricket-ground", 22 }, //22km
             { "kandalas", 29 },
             { "soni-cricket-ground-@jatt-xi", 30 },
             { "sadasiva-cricket-club-(scc)", 30},
-            { "rhb-cricket-ground", 30 },
             { "reet-cricket-club", 30 }, // 30 km
-            { "dcc-sports-arena", 10 }, // 9.5 km
             { "albatross-cricket-ground", 30 }, // 30 km
             { "smr-cricket-ground", 30 }, //30 km
             { "golconda-cricket-club", 30 }, // 30 km
@@ -67,7 +60,6 @@
             { "kbr-cricket-ground", 32 },
             { "hummingbird-cricket-stadium", 33},
             { "apr-cricket-ground", 34 },
-            { "cbr-cricket-ground", 34 },
             { "pvr-ground-tukuguda", 36 },
             { "urban-technology-farms-pt-ltd", 40 }, // 9km
             { "onechampion1-cricket-grounds", 40 }, //21 km
